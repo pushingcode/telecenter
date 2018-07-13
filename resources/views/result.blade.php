809c5a955fa4644616cdd936b7fdeba8c38f26a3
pushingcode <code_dev@zoho.com>
2018-07-13 17:44:21 -0400

    Ajuste de vista

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -10,7 +10,7 @@
           <div class="panel" style="width: 78rem;">
           <div class="panel-body">
             <h5 class="panel-title">Cuenta: {{$result->Numero_Cuenta}}</h5>
-            <h6 class="panel-subtitle mb-2 text-muted">Abonado: {{$result->Nombre}}</h6>
+            <a href="#" class="panel-link">Fecha: {{$result->Fecha}}</a>
             <span class="label label-primary">Orden: {{$result->Numero_Orden}}</span>
             <span class="label label-primary">{{$result->SubTipo_Orden}}</span>
             @php
@@ -21,10 +21,14 @@
               }
             @endphp
             <span class="label label-{{$label}}">{{$result->Estado}}</span>
-            <p class="panel-text">Comentario Ultima Orden: <br> {{$result->Notas_Entrantes}}</p>
-            <p class="panel-text">Tecnico: <br> {{$result->Tecnico}}</p>
-            <a href="#" class="panel-link">Ultima Visita: {{$result->Fecha}}</a>
-            <a href="#" class="panel-link">Telefono: {{$result->Telefono}}</a>
+            <p class="panel-text">Notas Entrantes: <br> {{$result->Notas_Entrantes}}</p>
+            <p class="panel-text">Notas de Cierre: <br>
+              @if($result->Habilidades_Trabajo == null)
+                No existen notas de cierre
+              @else
+                {{$result->Habilidades_Trabajo}}
+              @endif
+               </p>
           </div>
         </div>
         @endforeach
@@ -41,55 +45,8 @@
         @endphp
         </div>
 
-        <div class="panel" style="width: 78rem;">
-          <div id="map"></div>
-
-          <div class="panel-body">
-            <h5 class="panel-title">Direccion</h5>
-            <p class="panel-text">{{$result->Direccion}}</p>
-            <p class="text-info">Nodo: {{$result->Nodo}}</p>
-            <div>
-              <input id="address" type="hidden" value="{{$result->Direccion}}, {{$result->Ciudad}}, Argentina"> <input id="submit" type="button" value="Localizar">
-
-            </div>
-          </div>
-        </div>
-        <script>
-          function initMap() {
-            var map = new google.maps.Map(document.getElementById('map'), {
-              zoom: 8,
-              center: {lat: -34.6083, lng: -58.3712}
-            });
-            var geocoder = new google.maps.Geocoder();
-
-            document.getElementById('submit').addEventListener('click', function() {
-              geocodeAddress(geocoder, map);
-            });
-          }
-
-          function geocodeAddress(geocoder, resultsMap) {
-            var address = document.getElementById('address').value;
-            geocoder.geocode({'address': address}, function(results, status) {
-              if (status === 'OK') {
-                resultsMap.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
-                  map: resultsMap,
-                  position: results[0].geometry.location
-                });
-              } else {
-                alert('Geocode was not successful for the following reason: ' + status);
-              }
-            });
-          }
-        </script>
-
       </div>
     </div>
   </div>
-
-
-<script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDOZQRPFPuRw3EeoXwW_7v47ikP0F3JXeo&callback=initMap">
-</script>
 
 @endsection
